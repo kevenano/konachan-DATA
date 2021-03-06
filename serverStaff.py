6c289b9ac8450dad36fb02645a9938fadf1fcd7d
kevenano <kevenano@outlook.com>
2021-03-06 17:54:29 +0800

    First try.

--- serverStaff.py
+++ serverStaff.py
@@ -13,11 +13,26 @@
 from FUNCTION import download
 from CLASS import DB
 from apscheduler.schedulers.background import BackgroundScheduler
+import time
 
 
 # 业务部分代码
-
+def job():
+    print(int(time.time()))
 
 
 # 计划部分代码
+def schedulerPart():
+    '''
+    计划部分代码
+    '''
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=job,trigger='interval',seconds=3)
+    scheduler.start()
+    
 
+
+# 入口
+if __name__=="__main__":
+    schedulerPart()
+    time.sleep(100)
