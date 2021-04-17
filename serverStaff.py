02a5fcddbf76906003df78d40c61814059ec2394
kevenano <kevenano@outlook.com>
2021-04-17 18:08:47 +0800

    导入配置windows测试未通过

--- serverStaff.py
+++ serverStaff.py
@@ -11,42 +11,89 @@
 
 # Here put the import lib
 from CLASS import test
-from time import time,sleep
+from time import time, sleep
 from apscheduler.schedulers.background import BackgroundScheduler
 from Jobs import dailyJob
 import sys
 import os
 import cryptography
+import configparser
+from pprint import pprint
+import copy
+
 
 # 计划部分代码
-def schedulerPart(jobDir:str, testFlag:int=1):
+def schedulerPart(jobDir: str, dbPar: dict, mailPar: dict, trigerPar: dict, testFlag: int = 1):
     '''
     计划部分代码
     每天6点执行一次job中的业务
     '''
     scheduler = BackgroundScheduler()
-    # 目标函数参数设置
+    # 读入配置文件
     kwargs = {}
     kwargs['jobDir'] = jobDir
-    kwargs['host'] = 'localhost'
-    kwargs['user'] = 'kevin'
-    kwargs['passwd'] = '1972774684'
-    kwargs['database'] = 'konachan'
+    kwargs['dbPar'] = dbPar
+    kwargs['mailPar'] = mailPar
     if testFlag:
-        scheduler.add_job(func=dailyJob, kwargs=kwargs)
+        scheduler.add_job(func=dailyJob, kwargs=copy.copy(kwargs))
     else:
-        scheduler.add_job(func=dailyJob, trigger='cron', hour=6, kwargs=kwargs)
+        scheduler.add_job(func=dailyJob, trigger='cron',
+                          **trigerPar, kwargs=copy.copy(kwargs))
     scheduler.start()
-    
+
+
+def readConfig(confFile: str = 'server.conf'):
+    '''
+    读取配置文件
+    '''
+    cf = configparser.ConfigParser()
+    cf.read(confFile)
+
+    jobDir = ''
+    dbPar = {}
+    mailPar = {}
+    trigerPar = {}
+
+    if os.name == 'nt':
+        jobDir = cf.get('JOBDIR', 'windows')
+    else:
+        jobDir = cf.get('JOBDIR', 'linux')
+
+    dbPar['host'] = cf.get('MYSQL', 'host')
+    dbPar['port'] = cf.get('MYSQL', 'port')
+    dbPar['user'] = cf.get('MYSQL', 'user')
+    dbPar['passwd'] = cf.get('MYSQL', 'passwd')
+    dbPar['database'] = cf.get('MYSQL', 'database')
+
+    mailPar['sender'] = cf.get('MAIL', 'sender')
+    mailPar['passwd'] = cf.get('MAIL', 'passwd')
+    mailPar['receivers'] = cf.get('MAIL', 'receivers').split(',')
+
+    trigerPar['year'] = cf.get('TRIGER', 'year')
+    trigerPar['month'] = cf.get('TRIGER', 'month')
+    trigerPar['day'] = cf.get('TRIGER', 'day')
+    trigerPar['week'] = cf.get('TRIGER', 'week')
+    trigerPar['day_of_week'] = cf.get('TRIGER', 'day_of_week')
+    trigerPar['hour'] = cf.get('TRIGER', 'hour')
+    trigerPar['minute'] = cf.get('TRIGER', 'minute')
+
+    # pprint(jobDir)
+    # pprint(dbPar)
+    # pprint(mailPar)
+    # pprint(trigerPar)
+
+    return jobDir, dbPar, mailPar, trigerPar
+
 
 # 入口
-if __name__=="__main__":
+if __name__ == "__main__":
+    jobDir, dbPar, mailPar, trigerPar = readConfig()
     # 预处理
-    jobDir = r"/home/work"
     if not os.path.isdir(jobDir):
         os.makedirs(jobDir)
-    testFlag = 0
+    testFlag = 1
 
     # 启动服务器进程
-    schedulerPart(jobDir,testFlag)
-    sleep(31536000)
+    schedulerPart(jobDir, dbPar, mailPar, trigerPar, testFlag)
+    # sleep(31536000)
+    sleep(360)
